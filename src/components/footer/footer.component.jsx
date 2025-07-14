6b6d2fc8da91717de37849c6a57883c16063fb83
jcveli <jerald.velicaria@icloud.com>
2025-07-13 17:00:35 -0700

    Enhance footer styling: center content and adjust layout

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,9 +3,9 @@
 const Footer = () => {
 
     return (
-        <div class="bg-[#1c1f22] h-auto rounded-md shadow-inner p-4 w-full max-w-4xl mx-auto border border-[#2a2d30] mt-auto">
-            <div class="flex items-center">
-                <span class="text-1xl text-[#fdffff]">
+        <div class="bg-[#1c1f22] h-auto shadow-inner p-4 w-full mx-auto border border-[#2a2d30] mt-auto">
+            <div class="flex items-center justify-center">
+                <span class="text-1xl text-[#fdffff] text-center">
                     Page Last Updated: <span class="text-[#c2b6ff]">July 13, 2025</span> 
                 </span>
             </div>
